7983a2c295eab9a91f89e930a3c9175a526240e3
angelicamongaya8-design <angelicamongaya8@gmail.com>
2026-07-28 22:15:33 +0800

    Update booking-config.js

--- js/booking-config.js
+++ js/booking-config.js
@@ -1,3 +1,3 @@
 // config
 
-const BOOKING_ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbzfOFY4RzqePV-0Qv70ZjoCAE7hCgklPrK_12cJAjZ49lMnWHO_Lt28qqwbWILaX26K/exec";
+const BOOKING_ENDPOINT_URL = "https://ceamoto-backend.onrender.com";
